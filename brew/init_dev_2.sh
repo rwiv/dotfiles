30b73fad046ff9cd2acdfa884174739840a7b0cd
rwiv <zc798546@gmail.com>
2026-02-26 23:59:41 +0900

    feat: update rust plugin

--- brew/init_dev_2.sh
+++ brew/init_dev_2.sh
@@ -2,6 +2,7 @@ pipx install tldr
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup component add rust-analyzer
+cargo +stable install cargo-llvm-cov --locked
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
--- zsh/plugins/rust.zsh
+++ zsh/plugins/rust.zsh
@@ -1,8 +1,12 @@
 alias cof='cargo fmt'
 
+alias col='cargo clippy'
+
 alias coc='cargo check'
 alias cob='cargo build'
 alias cobr='cargo build --release'
 
 alias cot='cargo test'
+alias cotc='cargo llvm-cov'
+alias coth='cargo llvm-cov --html'
 
