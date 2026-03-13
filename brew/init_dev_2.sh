a7619ad0a278aa8beb62098f373068096c351d65
rwiv <zc798546@gmail.com>
2026-03-13 20:29:01 +0900

    feat: update init scripts

--- brew/init_dev_2.sh
+++ brew/init_dev_2.sh
@@ -1,13 +1,17 @@
-pipx install tldr
+#!/bin/sh
 
+# install rustup and rust-analyzer
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup component add rust-analyzer
 cargo +stable install cargo-llvm-cov --locked
 
+# install ohmyzsh and plugins
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# install tmux plugin manager and nvim config
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# install nvim config
 git clone https://github.com/rwiv/nvim.git ~/.config/nvim
---
+++ brew/init_dev_3.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+## npm
+npm install -g @google/gemini-cli
+
+npm install -g agent-browser
+agent-browser install # Download Chromium
+
+## pipx
+pipx install tldr
+pipx install markdownify
