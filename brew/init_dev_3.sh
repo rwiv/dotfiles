e45b9b7468400601d291bee6b38f38324b9e1b95
rwiv <zc798546@gmail.com>
2026-03-15 22:19:13 +0900

    feat: update init script

--- brew/init_dev_3.sh
+++ brew/init_dev_3.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-## nodejs
-npm install -g @google/gemini-cli
-
-npm install -g agent-browser
-agent-browser install
-
 ## python
 pipx install tldr
 
 pip3 install markdownify
 pip3 install playwright
 python3 -m playwright install
+
+## nodejs
+npm install -g agent-browser
+# agent-browser는 내부적으로 playwright를 사용한다. python3 -m playwright install로 이미 브라우저를 설치했기 때문에 agent-browser install를 수행할 필요는 없다.
+
+npm install -g @google/gemini-cli
