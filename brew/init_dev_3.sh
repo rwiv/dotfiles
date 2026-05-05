cd37b25ecc889125bb83edf344372dbe8f96f0e5
rwiv <zc798546@gmail.com>
2026-05-05 15:09:10 +0900

    feat: update init script

--- brew/init_dev_3.sh
+++ brew/init_dev_3.sh
@@ -7,4 +7,6 @@ pipx install tldr
 npm install -g agent-browser
 # agent-browser는 내부적으로 playwright을 사용한다. 만약 다른 프로젝트에서 uv run playwright install을 수행했다면, agent-browser install를 수행할 필요는 없다.
 
+curl -fsSL https://claude.ai/install.sh | bash
+npm install -g @openai/codex
 npm install -g @google/gemini-cli
